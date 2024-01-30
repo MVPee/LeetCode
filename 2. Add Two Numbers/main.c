c3c249d19b7d27da0522cdd3740e2630814d8c34
Marius Van Pee <mariusvanpee@hotmail.be>
2024-01-30 11:43:46 +0100

    start 2

---
+++ 2. Add Two Numbers/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 11:42:48 by mvan-pee          #+#    #+#             */
+/*   Updated: 2024/01/30 11:43:32 by mvan-pee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+    
+}
+
+int main(void)
+{
+    
+}
